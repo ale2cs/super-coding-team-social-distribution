1b416c9648ccce0ea17f39b583cf8a3d90fb73ff
bdlu1 <b.d.lu2002@gmail.com>
2023-10-21 13:00:22 -0600

    a bunch of changes

--- socialdistribution/social/models.py
+++ socialdistribution/social/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class Author(models.Model):
-    author_id = models.CharField(max_length=200)
-    host = models.CharField(max_length=200)
-    display =  models.CharField(max_length=200)
-    url = models.URLField(max_length=200)
-    github = models.CharField(max_length=200)
-    profile_image = models.IntegerField()
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+   
 
 class Post(models.Model):
     post_id = models.CharField(max_length=200)
--- socialdistribution/social/views.py
+++ socialdistribution/social/views.py
@@ -1,6 +1,86 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import RegisterUser, LoginUser, UpdateUserForm, UpdateProfileForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 # Create your views here.
-def index(request):
-    return HttpResponse("Social index")
+#def index(request):
+    #return HttpResponse("Social index")
+
+def home_page(request):
+    return render(request, 'home.html')
+
+class CustomLoginView(LoginView):
+    form_class = LoginUser
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get('remember_me')
+
+        if not remember_me:
+            # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
+            self.request.session.set_expiry(0)
+
+            # Set session as modified to force data updates/cookie to be saved.
+            self.request.session.modified = True
+
+        # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
+        return super(CustomLoginView, self).form_valid(form)
+    
+class register_profile(View):
+    form_class = RegisterUser
+    initial = {'key': 'value'}
+    template_name = 'register_profile.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # will redirect to the home page if a user tries to access the register page while logged in
+        if request.user.is_authenticated:
+            return redirect(to='/')
+
+        # else process dispatch as it otherwise normally would
+        return super(register_profile, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='/')
+
+        return render(request, self.template_name, {'form': form})
+    
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'change_password.html'
+    success_message = "Successfully Changed Your Password"
+    success_url = reverse_lazy('home')    
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'profile.html', {'user_form': user_form, 'profile_form': profile_form})
+    
