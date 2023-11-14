605e506dd106c74c5a36289d55870aef1f9793e1
ale2cs <ale2@ualberta.ca>
2023-11-14 16:12:37 -0700

    Add author API and reformat follower API to match specs

--- socialdistribution/api/urls.py
+++ socialdistribution/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('service/authors', views.Authors.as_view(), name='authors-list'),
+    path('service/authors/<str:author_id>', views.Author.as_view(), name='single-author'),
     path('service/authors/<str:author_id>/posts/<str:post_id>', views.PostDetail.as_view(), name='post-detail'),
     path('service/authors/<str:author_id>/posts', views.PostList.as_view(), name='post-list'),
     path('service/authors/<str:author_id>/followers', views.Followers.as_view(), name='followers'),
