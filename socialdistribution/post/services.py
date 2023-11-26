306d058a4190686a3b19f3ecf342d957b18eaea8
jvkchow <jechow@ualberta.ca>
2023-11-26 15:23:37 -0700

    fixed remote posts, added status_code to utils

--- socialdistribution/post/services.py
+++ socialdistribution/post/services.py
@@ -4,13 +4,13 @@ from api.utils import create_basic_auth_header, validate_response
 def get_posts_from_node(node, remote_author_id):
     response = requests.get(
             url=f"{remote_author_id}/posts",
-            headers=create_basic_auth_header(node.username, node.password)
+            headers=create_basic_auth_header(node.outbound_username, node.outbound_password)
         )
     return validate_response(response)
 
 def get_image_from_node(node, remote_post_id):
     response = requests.get(
             url=f"{remote_post_id}/image",
-            headers=create_basic_auth_header(node.username, node.password)
+            headers=create_basic_auth_header(node.outbound_username, node.outbound_password)
         )
     return validate_response(response)
